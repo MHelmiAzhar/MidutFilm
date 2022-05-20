68ff1f27cd80e22ef58b698678fcf1b077e8bc4c
MHelmiAzhar <helmiazhar311201@gmail.com>
2022-05-20 21:52:52 +0700

    add API

--- src/components/trending.js
+++ src/components/trending.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Card, Container, Row, Col, Image } from "react-bootstrap";
 import ItsOK from "../assets/film/film1.jpg";
 import PeakyBlinder from "../assets/film/film2.jpg";
@@ -5,39 +6,21 @@ import Spiderman from "../assets/film/film3.jpg";
 import Spongebob from "../assets/film/film4.jpg";
 import DoctorStrange from "../assets/film/film5.jpg";
 import KKN from "../assets/film/film6.jpg";
+import axios from "axios";
+
 const Trending = () => {
-  const dataTrending = [
-    {
-      image: ItsOK,
-      title: "It's Okay To Be Not Okay",
-      desc: "Its Okay To Not Be Okay mengisahkan tentang orang-orang yang haus cinta dan kasih sayang namun mengalami trauma dalam hidupnya. Kemudian saling bertemu dan menumbuhkan rasa satu sama lain dan berakhir dengan saling dukung serta saling cinta.",
-    },
-    {
-      image: PeakyBlinder,
-      title: "Peaky Blidnder",
-      desc: "Peaky Blinders adalah serial tentang keluarga gangster berlatar di Birmingham, Inggris tahun 1919. Mereka mengisahkan masa-masa setelah Perang Dunia Pertama meledak.",
-    },
-    {
-      image: Spiderman,
-      title: "Spiderman : No Way Home",
-      desc: "Spiderman No Way Home mengisahkan tentang kegelisahan Peter Parker yang diperankan Tom Holland karena difitnah. Mysterio, yang diperankan oleh Jake Gyllenhaal adalah orang yang memfitnah Parker dan akhirnya mati terlebih dahulu sebelum masalah bisa diselesaikan.",
-    },
-    {
-      image: Spongebob,
-      title: "SpongeBob Squarepants the Movie",
-      desc: "SpongeBob SquarePants mengambil cuti dari kota Bikini Bottom untuk melacak mahkota Raja Neptunus yang dicuri.",
-    },
-    {
-      image: DoctorStrange,
-      title: "Doctor Strange 2",
-      desc: "Doctor Strange 2 mengisahkan perjuangan Stephen Strange (Benedict Cumberbatch) menghadapi berbagai kejadian aneh akibat pintu lintas semesta yang terbuka pasca kejadian di Spider-Man No Way Home.",
-    },
-    {
-      image: KKN,
-      title: "KKN di Desa Penari",
-      desc: "mengisahkan kejadian mistis di luar logika yang menimpa enam mahasiswa kala kuliah kerja nyata (KKN) di desa terpencil.",
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BASE_URL}/discover/movie`, {
+        params: {
+          api_key: process.env.REACT_APP_TMDB_KEY,
+        },
+      })
+      .then((response) => {
+        setMovies(response.data.results);
+      });
+  }, []);
   return (
     <div>
       <Container>
@@ -46,18 +29,21 @@ const Trending = () => {
           Trending FIlm
         </h1>
         <br />
-        <Row className>
-          {dataTrending.map((trending, index) => (
-            <Col md={4}>
-              <div className="card">
-                <Card className="bg-dark text-white card">
-                  <Image src={trending.image} alt="ItsOk" className="img" />
-                  <Card.Title className="text-center p-2">{trending.title}</Card.Title>
-                  <Card.Text className="text-left p-2 desc">{trending.desc}</Card.Text>
-                </Card>
-              </div>
-            </Col>
-          ))}
+        <Row>
+          {movies.map((result, index) => {
+            return (
+              <Col md={4} key={index}>
+                <div className="card">
+                  <Card className="bg-dark text-white card">
+                    <Image src={`${process.env.REACT_APP_IMG_PATH}/${result.poster_path}`} alt="ItsOk" className="img" />
+                    <Card.Title className="text-center p-2">{result.title}</Card.Title>
+                    <Card.Title className="text-center">{result.release_date}</Card.Title>
+                    <Card.Text className="text-left p-2 desc">{result.overview}</Card.Text>
+                  </Card>
+                </div>
+              </Col>
+            );
+          })}
         </Row>
       </Container>
     </div>
